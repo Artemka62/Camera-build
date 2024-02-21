2ec5968db6278eea9b1768207d5f4d289ce1c6ed
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2024-02-21 15:04:51 +0400

    Внес правки в тесты согласно измненениям компонента FilterListCardsComponent

--- src/components/filter-list-cards/filter-list.test.tsx
+++ src/components/filter-list-cards/filter-list.test.tsx
@@ -3,13 +3,14 @@ import { store } from '../../store/store-index';
 import { withHistory } from '../mock-component/mock-component';
 import { render, screen } from '@testing-library/react';
 import { FilterListCardsComponent } from './filter-list-cards';
+import { mockOffer } from '../../mock-test/mock-offer/mock-offer';
 
 describe('component: FilterListCards', () => {
   it('should render correctly', () => {
     const expectedText = 'Фильтр';
     const preparedComponent = withHistory(
       <Provider store={store}>
-        <FilterListCardsComponent />
+        <FilterListCardsComponent offers={[mockOffer]} dataPriceMinMax={[mockOffer]}/>
       </Provider>
     );
 
