7bf3ec1166b633ceadfa3514487dff9eb66e4eca
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2024-02-21 21:35:00 +0400

    Исправил тест согласно изменениям компонента загрузки

--- src/components/loading-component/loading-component.tsx
+++ src/components/loading-component/loading-component.tsx
@@ -3,7 +3,7 @@ function LoadingComponent (): JSX.Element {
   return (
     <div className="loader-container">
       <div className="loader"></div>
-      <p className="loading-text">Загрузка...</p>
+      <p className="loading-text">Loading...</p>
     </div>
   );
 }
