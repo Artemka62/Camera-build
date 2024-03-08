b1202881a67d189fac8bc1f552779a6047a9fff4
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2024-03-08 13:32:54 +0400

    Реализовал управление кнопок в компоненте ModalWindowOrderSuccessComponent

--- src/components/modal-window-order-success/modal-window-order-success.tsx
+++ src/components/modal-window-order-success/modal-window-order-success.tsx
@@ -1,7 +1,74 @@
-import { useAppSelector } from '../../use-hooks/use-hook-store';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../use-hooks/use-hook-store';
+import { useEffect, useRef } from 'react';
+import { AppRoute, DEFAULT_NULL, DELAY_FOCUS } from '../../src-const';
+import { windowsSlice } from '../../store/slice/slice-modal-windows';
 
 function ModalWindowOrderSuccessComponent () {
   const isErrorLoading = useAppSelector((state) => state.order.error);
+  const navigate = useNavigate();
+  const returnToCatalogButtonRef = useRef<HTMLButtonElement>(null);
+  const closeButtonRef = useRef<HTMLButtonElement>(null);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    let isMounted = true;
+
+    if (returnToCatalogButtonRef.current && isMounted) {
+      setTimeout(() => {
+        returnToCatalogButtonRef.current?.focus();
+      }, DELAY_FOCUS);
+    }
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    if(isMounted){
+      document.body.classList.add('scroll-lock');
+    }
+
+    return () => {
+      document.body.classList.remove('scroll-lock');
+      isMounted = false;
+    };
+  }, []);
+
+  function pushDispatch () {
+    dispatch(windowsSlice.actions.isModalWindow(false));
+    dispatch(windowsSlice.actions.windowOrderSuccess(false));
+  }
+
+  function handleClickButtonClose () {
+    pushDispatch();
+  }
+
+  function handleClickButtonReturnToCatalog () {
+    pushDispatch();
+
+    navigate(`${AppRoute.Main}`);
+
+    window.scrollTo({
+      top: DEFAULT_NULL,
+      behavior: 'smooth'
+    });
+  }
+
+  function handlePressKeyReturnToCatalog (event:React.KeyboardEvent, nextInputRef: React.RefObject<HTMLButtonElement> | null) {
+    if(event.key === ' '){
+      event.preventDefault();
+    }
+
+    if (event.key === 'Tab' && nextInputRef && nextInputRef.current) {
+      event.preventDefault();
+      nextInputRef.current.focus();
+    }
+  }
+
 
   return(
     <div className="modal__content">
@@ -13,11 +80,21 @@ function ModalWindowOrderSuccessComponent () {
         <button
           className="btn btn--purple modal__btn modal__btn--fit-width"
           type="button"
+          ref={returnToCatalogButtonRef}
+          onClick={handleClickButtonReturnToCatalog}
+          onKeyDown={(event) => handlePressKeyReturnToCatalog(event, closeButtonRef)}
         >
           Вернуться к покупкам
         </button>
       </div>
-      <button className="cross-btn" type="button" aria-label="Закрыть попап">
+      <button
+        className="cross-btn"
+        type="button"
+        aria-label="Закрыть попап"
+        onKeyDown={(event) => handlePressKeyReturnToCatalog(event, returnToCatalogButtonRef)}
+        ref={closeButtonRef}
+        onClick={handleClickButtonClose}
+      >
         <svg width={10} height={10} aria-hidden="true">
           <use xlinkHref="#icon-close" />
         </svg>
