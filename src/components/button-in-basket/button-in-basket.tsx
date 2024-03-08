abc51c39e03a042093680d488266fac9c05427ee
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2024-03-08 13:48:59 +0400

    При клике кнопки в корзине перенаправил пользователя на страницу корзины

--- src/components/button-in-basket/button-in-basket.tsx
+++ src/components/button-in-basket/button-in-basket.tsx
@@ -1,14 +1,26 @@
+import { Link } from 'react-router-dom';
+import { AppRoute, DEFAULT_NULL } from '../../src-const';
+
 function ButtonInBasketComponent () {
+
+  function handleClickButtonInBasket () {
+    window.scrollTo({
+      top: DEFAULT_NULL,
+      behavior: 'smooth'
+    });
+  }
+
   return (
-    <a
+    <Link
+      to={AppRoute.Basket}
       className="btn btn--purple-border product-card__btn product-card__btn--in-cart"
-      href="#"
+      onClick={handleClickButtonInBasket}
     >
       <svg width={16} height={16} aria-hidden="true">
         <use xlinkHref="#icon-basket" />
       </svg>
       В корзине
-    </a>
+    </Link>
   );
 }
 
